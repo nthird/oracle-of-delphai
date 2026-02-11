669d2abc99a912bdca9e745ece5f08553f43fee1
nthird <ArkTherElf@yahoo.com>
2026-02-11 17:27:54 -0500

    adding images of spreads and some new spreads

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,18 @@ $(document).ready(function(){
 	$("#pastCardStatus").text("Empty");
 	$("#presentCardStatus").text("Empty");
 	$("#futureCardStatus").text("Empty");
+	$("#cardOneOfFiveStatus").text("Empty");
+	$("#cardTwoOfFiveStatus").text("Empty");
+	$("#cardThreeOfFiveStatus").text("Empty");
+	$("#cardFourOfFiveStatus").text("Empty");
+	$("#cardFiveOfFiveStatus").text("Empty");
+	$("#cardOneOfSevenStatus").text("Empty");
+	$("#cardTwoOfSevenStatus").text("Empty");
+	$("#cardThreeOfSevenStatus").text("Empty");
+	$("#cardFourOfSevenStatus").text("Empty");
+	$("#cardFiveOfSevenStatus").text("Empty");
+	$("#cardSixOfSevenStatus").text("Empty");
+	$("#cardSevenOfSevenStatus").text("Empty");
 	$("#submitTarotButton").hide();
 	$('input[name="time_card"]').prop('checked', false);
 	$("#promptReview").text("");
@@ -13,8 +25,101 @@ $(document).ready(function(){
 	$("#GPTResult").hide();
 	$("#submitTarotButtonGPT").hide();	
 	$("#resetButton").hide();	
+	
+	$("#threeCardSpreadTop").hide();
+	$("#fiveCardSpreadTop").hide();
+	$("#sevenCardSpreadTop").hide();
+	
+  $("input[name='spread_card']").change(function() {
+	var selectedSpread = $("input[type='radio'][name=spread_card]:checked", '#spreadRadio').val();
+	$("#suitRadio").hide();
+	$("#arcanaRadio").hide();
+	$("#wandsRadio").hide();
+	$("#cupsRadio").hide();
+	$("#swordsRadio").hide();
+	$("#pentaclesRadio").hide();
+	$("#positionRadio").hide();
+	$('input[name="suit_card"]').prop('checked', false);
+	$('input[name="suitRadio"]').prop('checked', false);
+	$("#timeRadio").hide();
+	$("#fiveCardRadio").hide();
+	$("#sevenCardRadio").hide();
+	$("#threeCardSpreadTop").hide();
+	$("#fiveCardSpreadTop").hide();
+	$("#sevenCardSpreadTop").hide();
+	if (selectedSpread == "Three") {		
+		$("#timeRadio").show();
+		$("#threeCardSpreadTop").show();
+	}
+	else if (selectedSpread == "Five Linear") {
+		$('#fiveCardSpreadImage').prop("src", "images/5cardlinear.png");
+		$("#cardOneOfFive").text("1 - Past");
+		$("#cardTwoOfFive").text("2 - Present");
+		$("#cardThreeOfFive").text("3 - Future");
+		$("#cardFourOfFive").text("4 - Advice");
+		$("#cardFiveOfFive").text("5 - Outcome");
+		$("#fiveCardRadio").show();
+		$("#fiveCardSpreadTop").show();
+	}
+	else if (selectedSpread == "Five Cross") {
+		$('#fiveCardSpreadImage').prop("src", "images/5cardcross.png");
+		$("#cardOneOfFive").text("1 - Challenge");
+		$("#cardTwoOfFive").text("2 - Past");
+		$("#cardThreeOfFive").text("3 - Present");
+		$("#cardFourOfFive").text("4 - Future");
+		$("#cardFiveOfFive").text("5 - Advice");
+		$("#fiveCardRadio").show();
+		$("#fiveCardSpreadTop").show();
+	}
+	else if (selectedSpread == "Seven Split") {
+		$('#sevenCardSpreadImage').prop("src", "images/7cardsplit.png");
+		$("#cardOneOfSeven").text("1 - Situation");
+		$("#cardTwoOfSeven").text("2 - Option A - Strength");
+		$("#cardThreeOfSeven").text("3 - Option A - Challenges");
+		$("#cardFourOfSeven").text("4 - Option A - Outcome");
+		$("#cardFiveOfSeven").text("5 - Option B - Strength");
+		$("#cardSixOfSeven").text("6 - Option B - Challanges");
+		$("#cardSevenOfSeven").text("7 - Option B - Outcome");
+		$("#sevenCardRadio").show();
+		$("#sevenCardSpreadTop").show();
+	}
+	else if (selectedSpread == "Seven Horseshoe") {
+		$('#sevenCardSpreadImage').prop("src", "images/7cardhorseshoe.png");
+		$("#cardOneOfSeven").text("1 - Past");
+		$("#cardTwoOfSeven").text("2 - Present");
+		$("#cardThreeOfSeven").text("3 - Future");
+		$("#cardFourOfSeven").text("4 - Attitude");
+		$("#cardFiveOfSeven").text("5 - Influences");
+		$("#cardSixOfSeven").text("6 - Obstacles");
+		$("#cardSevenOfSeven").text("7 - Outcomes");
+		$("#sevenCardRadio").show();
+		$("#sevenCardSpreadTop").show();
+	}
+  });
   
   $("input[name='time_card']").change(function() {
+	$("#arcanaRadio").hide();
+	$("#wandsRadio").hide();
+	$("#cupsRadio").hide();
+	$("#swordsRadio").hide();
+	$("#pentaclesRadio").hide();
+	$("#positionRadio").hide();
+	$('input[name="suit_card"]').prop('checked', false);
+    $("#suitRadio").show();
+  });
+  
+  $("input[name='five_card']").change(function() {
+	$("#arcanaRadio").hide();
+	$("#wandsRadio").hide();
+	$("#cupsRadio").hide();
+	$("#swordsRadio").hide();
+	$("#pentaclesRadio").hide();
+	$("#positionRadio").hide();
+	$('input[name="suit_card"]').prop('checked', false);
+    $("#suitRadio").show();
+  });
+  
+  $("input[name='seven_card']").change(function() {
 	$("#arcanaRadio").hide();
 	$("#wandsRadio").hide();
 	$("#cupsRadio").hide();
@@ -81,7 +186,17 @@ $(document).ready(function(){
   });
   
   $("#positionRadioButton").click(function(){
-	var currentTime = $("input[type='radio'][name=time_card]:checked", '#timeRadio').val();
+	var currentSpread = $("input[type='radio'][name=spread_card]:checked", '#spreadRadio').val();
+	var currentTime;
+	if (currentSpread == "Three") {
+	  currentTime = $("input[type='radio'][name=time_card]:checked", '#timeRadio').val();
+	}
+	else if (currentSpread == "Five Linear" || currentSpread == "Five Cross") {
+	  currentTime = $("input[type='radio'][name=five_card]:checked", '#fiveCardRadio').val();
+	}
+	else if (currentSpread == "Seven Split" || currentSpread == "Seven Horseshoe") {
+	  currentTime = $("input[type='radio'][name=seven_card]:checked", '#sevenCardRadio').val();
+	}
 	var currentSuit = $("input[type='radio'][name=suit_card]:checked", '#suitRadio').val();
 	var currentValue = "";
 	if (currentSuit == "Arcana") {
@@ -110,18 +225,75 @@ $(document).ready(function(){
 		combinedCard = currentValue + " of " + currentSuit + " " + currentPosition;
 	}
 	
-	if (currentTime == "Past") {
-		$("#pastCardStatus").text(combinedCard);
+	if (currentSpread == "Three") {
+		if (currentTime == "Past") {
+			$("#pastCardStatus").text(combinedCard);
+		}
+		else if (currentTime == "Present") {
+			$("#presentCardStatus").text(combinedCard);
+		}
+		else if (currentTime == "Future") {
+			$("#futureCardStatus").text(combinedCard);
+		}
 	}
-	else if (currentTime == "Present") {
-		$("#presentCardStatus").text(combinedCard);
+	else if (currentSpread == "Five Linear" || currentSpread == "Five Cross") {
+		if (currentTime == "One") {
+			$("#cardOneOfFiveStatus").text(combinedCard);
+		}
+		else if (currentTime == "Two") {
+			$("#cardTwoOfFiveStatus").text(combinedCard);
+		}
+		else if (currentTime == "Three") {
+			$("#cardThreeOfFiveStatus").text(combinedCard);
+		}
+		else if (currentTime == "Four") {
+			$("#cardFourOfFiveStatus").text(combinedCard);
+		}
+		else if (currentTime == "Five") {
+			$("#cardFiveOfFiveStatus").text(combinedCard);
+		}
 	}
-	else if (currentTime == "Future") {
-		$("#futureCardStatus").text(combinedCard);
+	else if (currentSpread == "Seven Split" || currentSpread == "Seven Horseshoe") {
+		if (currentTime == "One") {
+			$("#cardOneOfSevenStatus").text(combinedCard);
+		}
+		else if (currentTime == "Two") {
+			$("#cardTwoOfSevenStatus").text(combinedCard);
+		}
+		else if (currentTime == "Three") {
+			$("#cardThreeOfSevenStatus").text(combinedCard);
+		}
+		else if (currentTime == "Four") {
+			$("#cardFourOfSevenStatus").text(combinedCard);
+		}
+		else if (currentTime == "Five") {
+			$("#cardFiveOfSevenStatus").text(combinedCard);
+		}
+		else if (currentTime == "Six") {
+			$("#cardSixOfSevenStatus").text(combinedCard);
+		}
+		else if (currentTime == "Seven") {
+			$("#cardSevenOfSevenStatus").text(combinedCard);
+		}
 	}
 	
-	if ($("#pastCardStatus").text() != "Empty" && $("#presentCardStatus").text() != "Empty" && $("#futureCardStatus").text() != "Empty") {
-		$("#submitTarotButton").show();
+	if (currentSpread == "Three") {
+		if ($("#pastCardStatus").text() != "Empty" && $("#presentCardStatus").text() != "Empty" && $("#futureCardStatus").text() != "Empty") {
+			$("#submitTarotButton").show();
+		}
+	}
+	else if (currentSpread == "Five Linear" || currentSpread == "Five Cross") {
+		if ($("#cardOneOfFiveStatus").text() != "Empty" && $("#cardTwoOfFiveStatus").text() != "Empty" && $("#cardThreeOfFiveStatus").text() != "Empty" &&
+				$("#cardFourOfFiveStatus").text() != "Empty" && $("#cardFiveOfFiveStatus").text() != "Empty")  {
+			$("#submitTarotButton").show();
+		}
+	}
+	else if (currentSpread == "Seven Split" || currentSpread == "Seven Horseshoe") {
+		if ($("#cardOneOfSevenStatus").text() != "Empty" && $("#cardTwoOfSevenStatus").text() != "Empty" && $("#cardThreeOfSevenStatus").text() != "Empty" &&
+				$("#cardFourOfSevenStatus").text() != "Empty" && $("#cardFiveOfSevenStatus").text() != "Empty" && $("#cardSixOfSevenStatusvenStatus").text() != "Empty"  &&
+				$("#cardSevenOfSevenStatus").text() != "Empty") {
+			$("#submitTarotButton").show();
+		}
 	}
 	
 	$('input[name="time_card"]').prop('checked', false);
@@ -136,23 +308,145 @@ $(document).ready(function(){
   });
   
   $("#submitTarotButton").click(async function(){
+	  var submitSpread = $("input[type='radio'][name=spread_card]:checked", '#spreadRadio').val();
 	  // Initialize an empty array as the buffer
 	  const buffer = [];
+	  
+	  if (submitSpread == "Three") {
 	 
-	  // Append strings to the buffer (O(1) per push)
-	  buffer.push("Interpret a tarot reading for me with the following specifications. ");
-	  buffer.push("I will provide information about what cards I drew. ");
-	  buffer.push("The cards are in a 3 card spread, with the first card representing the past, ");
-	  buffer.push("the second card representing the present, and the third card representing the future. ");
-	  buffer.push("First, write a brief poem about the reading. ");
-	  buffer.push("Second, tell me what my reading is and what it means, card by card. ");
-	  buffer.push("The first card in this reading is ");
-	  buffer.push($("#pastCardStatus").text());
-	  buffer.push(". The second card is ");
-	  buffer.push($("#presentCardStatus").text()); 
-	  buffer.push(". The third card is ");
-	  buffer.push($("#futureCardStatus").text());
-	  buffer.push(".");
+		  // Append strings to the buffer (O(1) per push)
+		  buffer.push("Interpret a tarot reading for me with the following specifications. ");
+		  buffer.push("I will provide information about what cards I drew. ");
+		  buffer.push("The cards are in a 3 card spread, with the first card representing the past, ");
+		  buffer.push("the second card representing the present, and the third card representing the future. ");
+		  buffer.push("First, write a brief poem about the reading. ");
+		  buffer.push("Second, tell me what my reading is and what it means, card by card. ");
+		  buffer.push("The first card in this reading is ");
+		  buffer.push($("#pastCardStatus").text());
+		  buffer.push(". The second card is ");
+		  buffer.push($("#presentCardStatus").text()); 
+		  buffer.push(". The third card is ");
+		  buffer.push($("#futureCardStatus").text());
+		  buffer.push(".");
+	  }
+	  
+	  else if (submitSpread == "Five Linear") {
+	 
+		  // Append strings to the buffer (O(1) per push)
+		  buffer.push("Interpret a tarot reading for me with the following specifications. ");
+		  buffer.push("I will provide information about what cards I drew. ");
+		  buffer.push("The cards are in a 5 card spread. ");
+		  buffer.push("The first card represents the Past. ");
+		  buffer.push("The second card represents the Present. ");
+		  buffer.push("The third card represents the Future. ");
+		  buffer.push("The fourth card represents Advice. ");
+		  buffer.push("The fifth card represents Outcomes. ");
+		  buffer.push("First, write a brief poem about the reading. ");
+		  buffer.push("Second, tell me what my reading is and what it means, card by card. ");
+		  buffer.push("The first card in this reading is ");
+		  buffer.push($("#cardOneOfFiveStatus").text());
+		  buffer.push(". The second card is ");
+		  buffer.push($("#cardTwoOfFiveStatus").text()); 
+		  buffer.push(". The third card is ");
+		  buffer.push($("#cardThreeOfFiveStatus").text());
+		  buffer.push(". The fourth card is ");
+		  buffer.push($("#cardFourOfFiveStatus").text());
+		  buffer.push(". The fifth card is ");
+		  buffer.push($("#cardFiveOfFiveStatus").text());
+		  buffer.push(".");
+	  }
+	  
+	  else if (submitSpread == "Five Cross") {
+	 
+		  // Append strings to the buffer (O(1) per push)
+		  buffer.push("Interpret a tarot reading for me with the following specifications. ");
+		  buffer.push("I will provide information about what cards I drew. ");
+		  buffer.push("The cards are in a 5 card spread and are arranged in a cross pattern. ");
+		  buffer.push("Card one is on the top row alone, cards two, three, and four are in the middle row in order, and card 5 is on the bottom row alone." );
+		  buffer.push("The first card represents the Challenge. ");
+		  buffer.push("The second card represents the Past. ");
+		  buffer.push("The third card represents the Present. ");
+		  buffer.push("The fourth card represents the Future. ");
+		  buffer.push("The fifth card represents Advice. ");
+		  buffer.push("First, write a brief poem about the reading. ");
+		  buffer.push("Second, tell me what my reading is and what it means, card by card. ");
+		  buffer.push("The first card in this reading is ");
+		  buffer.push($("#cardOneOfFiveStatus").text());
+		  buffer.push(". The second card is ");
+		  buffer.push($("#cardTwoOfFiveStatus").text()); 
+		  buffer.push(". The third card is ");
+		  buffer.push($("#cardThreeOfFiveStatus").text());
+		  buffer.push(". The fourth card is ");
+		  buffer.push($("#cardFourOfFiveStatus").text());
+		  buffer.push(". The fifth card is ");
+		  buffer.push($("#cardFiveOfFiveStatus").text());
+		  buffer.push(".");
+	  }
+	  
+	  else if (submitSpread == "Seven Split") {
+	 
+		  // Append strings to the buffer (O(1) per push)
+		  buffer.push("Interpret a tarot reading for me with the following specifications. ");
+		  buffer.push("I will provide information about what cards I drew. ");
+		  buffer.push("The cards are in a 7 card spread. ");
+		  buffer.push("The first card represents the Situation. ");
+		  buffer.push("The second card represents the Strengths of Option A. ");
+		  buffer.push("The third card represents the Challenges of Option A. ");
+		  buffer.push("The fourth card represents outcomes of Option A. ");
+		  buffer.push("The fifth card represents the Strengths of Option B. ");
+		  buffer.push("The sixth card represents the Challenges of Option B. ");
+		  buffer.push("The seventh card represents outcomes of Option B. ");
+		  buffer.push("First, write a brief poem about the reading. ");
+		  buffer.push("Second, tell me what my reading is and what it means, card by card. ");
+		  buffer.push("The first card in this reading is ");
+		  buffer.push($("#cardOneOfSevenStatus").text());
+		  buffer.push(". The second card is ");
+		  buffer.push($("#cardTwoOfSevenStatus").text()); 
+		  buffer.push(". The third card is ");
+		  buffer.push($("#cardThreeOfSevenStatus").text());
+		  buffer.push(". The fourth card is ");
+		  buffer.push($("#cardFourOfSevenStatus").text());
+		  buffer.push(". The fifth card is ");
+		  buffer.push($("#cardFiveOfSevenStatus").text());
+		  buffer.push(". The sixth card is ");
+		  buffer.push($("#cardSixOfSevenStatus").text());
+		  buffer.push(". The seventh card is ");
+		  buffer.push($("#cardSevenOfSevenStatus").text());
+		  buffer.push(".");
+	  }
+	  
+	  else if (submitSpread == "Seven Horseshoe") {
+	 
+		  // Append strings to the buffer (O(1) per push)
+		  buffer.push("Interpret a tarot reading for me with the following specifications. ");
+		  buffer.push("I will provide information about what cards I drew. ");
+		  buffer.push("The cards are in a 7 card spread. ");
+		  buffer.push("The first card represents the Past. ");
+		  buffer.push("The second card represents the Present. ");
+		  buffer.push("The third card represents the Future. ");
+		  buffer.push("The fourth card represents the Attitude of the asker towards the question. ");
+		  buffer.push("The fifth card represents Influences. ");
+		  buffer.push("The sixth card represents Obstacles. ");
+		  buffer.push("The seventh card represents Outcomes. ");
+		  buffer.push("First, write a brief poem about the reading. ");
+		  buffer.push("Second, tell me what my reading is and what it means, card by card. ");
+		  buffer.push("The first card in this reading is ");
+		  buffer.push($("#cardOneOfSevenStatus").text());
+		  buffer.push(". The second card is ");
+		  buffer.push($("#cardTwoOfSevenStatus").text()); 
+		  buffer.push(". The third card is ");
+		  buffer.push($("#cardThreeOfSevenStatus").text());
+		  buffer.push(". The fourth card is ");
+		  buffer.push($("#cardFourOfSevenStatus").text());
+		  buffer.push(". The fifth card is ");
+		  buffer.push($("#cardFiveOfSevenStatus").text());
+		  buffer.push(". The sixth card is ");
+		  buffer.push($("#cardSixOfSevenStatus").text());
+		  buffer.push(". The seventh card is ");
+		  buffer.push($("#cardSevenOfSevenStatus").text());
+		  buffer.push(".");
+	  }
+	 
 	 
 	  // Convert buffer to final string (O(n) time)
 	  const result = await buffer.join(""); 
@@ -210,6 +504,18 @@ $(document).ready(function(){
 		$("#pastCardStatus").text("Empty");
 		$("#presentCardStatus").text("Empty");
 		$("#futureCardStatus").text("Empty");
+		$("#cardOneOfFiveStatus").text("Empty");
+		$("#cardTwoOfFiveStatus").text("Empty");
+		$("#cardThreeOfFiveStatus").text("Empty");
+		$("#cardFourOfFiveStatus").text("Empty");
+		$("#cardFiveOfFiveStatus").text("Empty");
+		$("#cardOneOfSevenStatus").text("Empty");
+		$("#cardTwoOfSevenStatus").text("Empty");
+		$("#cardThreeOfSevenStatus").text("Empty");
+		$("#cardFourOfSevenStatus").text("Empty");
+		$("#cardFiveOfSevenStatus").text("Empty");
+		$("#cardSixOfSevenStatus").text("Empty");
+		$("#cardSevenOfSevenStatus").text("Empty");
 		$("#submitTarotButton").hide();
 		$('input[name="time_card"]').prop('checked', false);
 		$("#promptReview").text("");
